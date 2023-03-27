c4030cba5a62c82e33d6d2d36b27f7d97e2904f8
Denis Bakharev <java.bakharev@gmail.com>
2023-03-27 09:45:44 +0300

    correct style

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,19 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean1 =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean1.getMessage());
-    HelloWorld bean2 =
-            (HelloWorld) applicationContext.getBean("helloworld");
-    Cat cat1 =
-            (Cat) applicationContext.getBean("cat");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
         System.out.println(cat1.getName());
-    Cat cat2 =
-            (Cat) applicationContext.getBean("cat");
-System.out.println("bean1 == bean2: " + (bean1 == bean2));
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("bean1 == bean2: " + (bean1 == bean2));
         System.out.println("cat1 == cat2: " + (cat1 == cat2));
     }
 }
